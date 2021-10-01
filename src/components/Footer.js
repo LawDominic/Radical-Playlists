69649cc641b34814151814c33cf6a1edee48f42a
Dominic Law <dominic.law79@gmail.com>
2021-10-01 19:16:03 +1000

    Add footer padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { NavLink, Route, Switch } from "react-router-dom";
 function Footer() {
     return (
         <div class="bg-black fixed bottom-0 w-full text-white">
-        <div class="flex flex-col items-center justify-between mx-auto mt-3 md:flex-row container">
+        <div class="flex flex-col items-center justify-between mx-auto py-2 md:flex-row container">
             <div>&copy; 2021 Radical Playlists.</div>
             <div class="flex mt-4 md:m-0">
             <NavLink exact to={"/"} className="px-4 text-sm hover:text-green-500">
