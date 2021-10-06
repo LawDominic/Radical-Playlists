8fc1df51e4ed4039ed2659784ffcc9b5084e60ae
Dominic Law <dominic.law79@gmail.com>
2021-10-06 21:05:31 +1100

    Remove Playlist component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import Footer from "./components/Footer"
 import NavBar from "./components/NavBar"
-import Playlists from "./components/Playlists"
 
 function App() {
   return (
     <div class="min-h-screen h-full bg-gray-100 font-gotham">
       <NavBar />
-      <div class="container mx-auto">
-        <Playlists />
-      </div>
       <Footer />
     </div>
   );
