210295fba8b871dce68273cac1f176d0117a76d6
Dominic Law <dominic.law79@gmail.com>
2021-10-12 21:57:24 +1100

    Add Upload page

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import UserPlaylists from './UserPlaylists';
 
 function Upload() {
 
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    return (
+        <div className="container mx-auto">
+            {/* {isLoggedIn ?
+                <UserPlaylists /> :
+                <div className="text-center">You're currently not logged in. Please do so to access this feature.</div>
+            } */}
+            <UserPlaylists />
+        </div>
+    )
 }
 
 export default Upload
