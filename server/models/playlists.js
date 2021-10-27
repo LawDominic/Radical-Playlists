94308bcfa01e93ecdc0f4792d20480858b635582
Jacob Johnston <gounggoung@hotmail.com>
2021-10-27 21:04:16 +1100

    Add user schema
    Co-authored-by: Neil R <NeilRoberts7@users.noreply.github.com>

--- server/models/playlists.js
+++ server/models/playlists.js
@@ -1,32 +1,50 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-const url = process.env.DB_HOST
+const url = process.env.DB_HOST;
 
-console.log('connecting to', url)
+console.log("connecting to", url);
 
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(result => {
-        console.log("connected to MongoDB")
-    })
-    .catch((error) => {
-        console.log("error connecting to MongoDB:", error.message)
-    })
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 const playlistSchema = new mongoose.Schema({
-    playlistID: String,
-    timestamp: Date,
-    likes: Number
-})
+  playlistID: String,
+  timestamp: Date,
+  likes: Number,
+});
 
-playlistSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+const userSchema = new mongoose.Schema({
+  userID: String,
+  favoritePlaylists: Array,
+});
 
+playlistSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Playlist', playlistSchema)
+const Playlist = mongoose.model("Playlist", playlistSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+    User: User,
+    Playlist: Playlist
+}
