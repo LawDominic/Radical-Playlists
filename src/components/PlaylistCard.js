509ca62b4fa6ad875f9dadc003b0f92dc13cd94e
Jacob Johnston <gounggoung@hotmail.com>
2021-11-05 18:33:38 +1100

    Added missing useEffect import back

--- src/components/PlaylistCard.js
+++ src/components/PlaylistCard.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { PlusCircleIcon } from "@heroicons/react/outline";
 import { BookmarkIcon } from "@heroicons/react/outline";
 import { BookmarkIcon as BookmarkSolidIcon } from "@heroicons/react/solid";
