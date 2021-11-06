72a46b697bb76bf103c26d4b8068bf34f904468f
Dominic Law <dominic.law79@gmail.com>
2021-11-06 22:56:49 +1100

    Add Delete Data to mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+
+import { ShieldExclamationIcon } from "@heroicons/react/outline";
 
 import User from "./User"
 
 import pImg from "../images/defaultprofile.png";
 import logo from "../images/logo.png";
 
+import spotifyService from "../services/spotifyService";
 import { loginUrl } from '../services/spotifyService'
 
 function NavBar({user, isLoggedIn}) {
+  const spotifyURL = "https://open.spotify.com/user/"
+  const [showUser, setShowUser] = useState(false);
+  const [deleteModal, setDeleteModal] = useState(false)
+
+  const deleteAccount = (e) => {
+    spotifyService.deleteUser(user.id);
+    window.location.reload()
+}
+
   const mobileMenuHandler = (e) => {
     const btn = document.querySelector("button.mobile-menu-button");
     const menu = document.querySelector(".mobile-menu");
@@ -96,13 +108,18 @@ function NavBar({user, isLoggedIn}) {
               <NavLink to="/upload" className="block text-sm px-2 py-4 text-white font-semibold hover:bg-gray-900 hover:text-green-500 transition duration-300">
                 Upload playlists
               </NavLink>
-              <a href="/" className="block text-sm px-2 py-4 text-white font-semibold hover:bg-gray-900 hover:text-green-500 transition duration-300">
-                Spotify Profile
-              </a>
-              {isLoggedIn ? 
-                <NavLink to="/logout" className="block text-sm px-2 py-4 text-white font-semibold hover:bg-gray-900 hover:text-red-500 transition duration-300">
+              {isLoggedIn ?
+              <div>
+                <a target="_blank"rel="noreferrer"href={spotifyURL + user.id} className="block text-sm px-2 py-4 text-white font-semibold hover:bg-gray-900 hover:text-green-500 transition duration-300">
+                  Spotify Profile
+                </a>
+                <a to="/" className="block text-sm px-2 py-4 text-white font-semibold hover:bg-gray-900 hover:text-red-500 transition duration-300">
                   Logout
-                </NavLink>
+                </a>
+                <p href="/" className="block text-sm px-4 py-2 text-white font-semibold hover:text-red-500" onClick={() =>{setDeleteModal(!deleteModal)}}>
+                    Delete Data
+                </p>
+              </div>
                 :
                 <a href={loginUrl} className="block text-sm px-2 py-4 text-white font-semibold hover:bg-gray-900 hover:text-green-500 transition duration-300">
                   Login
@@ -110,7 +127,30 @@ function NavBar({user, isLoggedIn}) {
               }
             </ul>
           </div>
-
+          {deleteModal ? 
+            <div class="flex flex-col space-y- min-w-screen h-screen animated fadeIn faster fixed left-0 top-0 justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-gray-900 bg-opacity-90 px-2">
+                <div class="flex flex-col p-8 bg-white rounded-2xl">
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center">
+                            <ShieldExclamationIcon className="h-12 w-12 text-red-600 rounded-xl bg-red-100 p-2"/>
+                            <div class="flex flex-col ml-3">
+                                <div class="font-medium leading-none">Confirmation</div>
+                                <p class="text-sm text-gray-600 leading-none mt-1">By deleting your account you will lose all your data.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex mx-auto items-center justify-center space-x-2 mt-6">
+                        <button onClick={() => {setDeleteModal(false)}} className="bg-gray-100 px-5 py-2 text-gray-900 rounded-full hover:bg-gray-200 shadow-md">
+                            Cancel
+                        </button>
+                        <button onClick={deleteAccount} className="bg-red-500 px-5 py-2 text-white rounded-full hover:bg-red-600 shadow-md">
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            </div>
+            : null}
         </div>
       </div>
     </div>
